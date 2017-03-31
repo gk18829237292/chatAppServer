d3bdee2a580650e4fbdf584226a19839f067b16e
ke.gao <vipgaoke@163.com>
2017-03-31 20:53:18 +0800

    little change

---
+++ app/userDao.js
@@ -0,0 +1,96 @@
+﻿module.exports = function() {
+	var mysql = require('mysql');
+	
+	var userEntry = function(account,nickName,sex){
+		this.account = account;
+		this.nickName = nickName;
+		this.sex = sex;
+	}
+	
+	function createConnection(){
+		var connection = mysql.createConnection({
+				host: 'localhost',
+				user: 'root',
+				password: '7320',
+				database:'chatApp'
+			});
+		connection.connect();
+		return connection;
+	}
+	
+	return{
+		getAllUser:function(callback){
+			var connection = createConnection();
+			connection.query('select * from user',function(err,rows,fields){
+				if(err) throw err;
+				var userList=[];
+				rows.forEach(function(item,index){
+					var user= new userEntry(item.account,item.nickname,item.sex);
+					userList.push(user);
+				});
+				callback(userList);
+			});
+			connection.end();
+		},
+		
+		insertUser:function(account,password,nickName,sex,callback){
+			var conn = createConnection();
+			var param = [account,password,nickName,sex];
+			conn.query("insert into user values(?,?,?,?)",param,function(err,rows,fields){
+				if(err) throw err;
+				callback(true);
+			});
+			conn.end();
+		},
+		
+		deleteUser:function(account,callback){
+			var conn = createConnection();
+			var param = [account];
+			conn.query("delete from user where account = ?",param,function(err,rows,fields){
+				if(err) throw err;
+				callback();
+			});
+			conn.end();
+		},
+		updateUserNickName:function(account,nickName,callback){
+			var conn = createConnection();
+			var param = [nickName,account];
+			conn.query("update user set nickname = ? where account = ?",param,function(err,rows,fields){
+				if(err) throw err;
+				callback();
+			});
+			conn.end();
+		},
+		updateUserSex:function(account,sex,callback){
+			var conn = createConnection();
+			var param = [sex,account];
+			conn.query("update user set sex = ? where account = ?",param,function(err,rows,fields){
+				if(err) throw err;
+				callback();
+			});
+			conn.end();
+		},
+		updateUser:function(account,nickName,sex,callback){
+			var conn = createConnection();
+			var param = [nickName,sex,account];
+			conn.query("update user set nickname=?,sex = ? where account = ?",param,function(err,rows,fields){
+				if(err) throw err;
+				callback();
+			});
+			conn.end();
+		},
+		login:function(account,password,callback){
+			var conn = createConnection();
+			var param = [account,password,account];
+			conn.query("select * from user where account = ? and password =?",param,function(err,rows,fields){
+				if(err) throw err;
+				if(rows.length == 1){
+					callback(true);
+				}else{
+					callback(false);
+				}
+			});
+			conn.end();
+		}
+	}
+}
--- index.js
+++ index.js
@@ -2,19 +2,48 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var db = require('./app/userDao.js')();
 
-
+var onLineUser=[];
+var userList={};
 
 io.on('connection', function(socket){
 	console.log('-- ' + socket.id + ' joined --');
-    
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  
-  socket.on('register',function(account,password,nickname,sex){
-	console.log('---' + account + ' '+ password + ' ' + nickname + ' ' + sex);
+    userList[socket.id] = '1212';
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
 	});
+  
+	socket.on('register',function(account,password,nickname,sex){
+		db.insertUser(account,password,nickname,sex,function(result){
+			socket.emit('register_result',result);
+		});
+	});
+	
+	socket.on('login',function(account,password){
+		
+		db.login(account,password,function(result){
+			if(result){
+				console.log('-- ' + account + ' login success');
+				onLineUser.push(account);
+				var key = socket.id;
+				userList[socket.id] = account;
+				console.log(userList);
+			}else{
+				console.log('-- ' + account + ' login failed');
+			}
+			socket.emit('login_result',result);
+		});
+	});
+	
+	socket.on('disconnect',function(){
+		console.log(onLineUser);
+		console.log(userList);
+		delete(userList[socket.id]);
+		console.log(userList);
+	});
+	
+	
 });
 
 http.listen(port, function(){
